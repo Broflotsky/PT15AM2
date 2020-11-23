918e3a10afc6971d134272ab877125d405667f42
agusamani <amanijuanagustin@gmail.com>
2020-11-23 15:35:08 -0300

    demos changes

--- 08-React-Estado-LifeCycle/demo/webpack/app.js
+++ 08-React-Estado-LifeCycle/demo/webpack/app.js
@@ -4,33 +4,7 @@ import { render } from 'react-dom';
 import Contador from './src/components/Contador.jsx';
 import Main from './src/containers/Main.jsx';
 
-// const element = <h1>Hello, world!</h1>;
-
-// render(element,  document.getElementById('app'));
-
-
-
-// class Welcome extends React.Component {
-//  render() {
-//    return <h1>Hello, {this.props.name}</h1>;
-//  }
-// }
-  
-// function WelcomeFuncional(props) {
-//   return <h1>Hello, {props.name}</h1>;
-// }
-
-
-// render((<div>
-//   <Welcome name="Henry"/>
-//   <WelcomeFuncional name="Henry Funcional" />
-//   </div>
-//  )
-//  ,  document.getElementById('app'));
-
-
-// render(<Contador contador={0}/>, document.getElementById('app'));
-
+render(<Contador contador={0}/>, document.getElementById('app'));
 
 const products = [
   { title: 'Uno', price: 1},
@@ -39,4 +13,4 @@ const products = [
   { title: 'Cuatro', price: 4},
 ];
 
-render(<Main products={products}/>, document.getElementById('app'));
+// render(<Main products={products}/>, document.getElementById('app'));
