de6bce9e995a93049ed28522cf6a8502cf5dbb79
Franverri <francoetche@gmail.com>
2021-04-09 17:03:27 -0300

    Fix update 10

--- 10-React-Forms/homework/src/App.test.js
+++ 10-React-Forms/homework/src/App.test.js
@@ -1,33 +1,23 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent, screen, getByTestId} from '@testing-library/react';
 import { shallow, mount } from 'enzyme';
 import Form, {validate}  from './Form.jsx';
 
-describe('<Form />', () => {
+describe('App', () => {
   let wrapper;
-  const setState = jest.fn();
-  const useStateSpy = jest.spyOn(React, 'useState')
-  useStateSpy.mockImplementation((init) => [init, setState]);
-
   beforeEach(() => {
-    wrapper = shallow(<Form />);
+    wrapper = mount(<Form />);
   });
-
-  afterEach(() => {
-    jest.clearAllMocks();
+  it('El form deberia cambiar de estado cuando escriban en el input de username', () => {
+    wrapper.find('input[name="username"]').simulate('change', {target: {name: 'username', value: 'NewUsr'}});
+    const ele = wrapper.find('input[name="username"]');
+    expect(ele.prop('value')).toEqual('NewUsr');
   });
-
-  describe('Estados: ', () => {
-    it('El form deberia cambiar de estado cuando escriban en el input de username', () => {
-      wrapper.find('input[name="username"]').simulate('change', {target: {name: 'username', value: 'My new value'}});
-      expect(setState).toHaveBeenCalledWith({"username": "My new value", "password": ""});
-    });
-    it('El form deberia cambiar de estado cuando escriban en el input de password', () => {
-      wrapper.find('input[name="username"]').simulate('change', {target: {name: 'password', value: 'My new value'}});
-      expect(setState).toHaveBeenCalledWith({"username": "", "password": "My new value"});
-    });
+  it('El form deberia cambiar de estado cuando escriban en el input de password', () => {
+    wrapper.find('input[name="password"]').simulate('change', {target: {name: 'password', value: 'NewPsw'}});
+    const ele = wrapper.find('input[name="password"]');
+    expect(ele.prop('value')).toEqual("NewPsw");
   });
-
   describe('Validacion: ', () => {
     it('validate debe devolver un objeto con un error si el usarname no es un email valido:', () => {
       expect(validate({
@@ -56,4 +46,56 @@ describe('<Form />', () => {
   });
 });
 
-
+// describe('<Form />', () => {
+//   let wrapper;
+//   const setState = jest.fn();
+//   const useStateSpy = jest.spyOn(React, 'useState')
+//   useStateSpy.mockImplementation((init) => [init, setState]);
+//
+//   beforeEach(() => {
+//     wrapper = shallow(<Form />);
+//   });
+//
+//   afterEach(() => {
+//     jest.clearAllMocks();
+//   });
+//
+//   describe('Estados: ', () => {
+//     it('El form deberia cambiar de estado cuando escriban en el input de username', () => {
+//       wrapper.find('input[name="username"]').simulate('change', {target: {name: 'username', value: 'My new value'}});
+//       console.log(wrapper.find('input[name="username"]'));
+//       expect(setState).toHaveBeenCalledWith({"username": "My new value", "password": ""});
+//     });
+//     it('El form deberia cambiar de estado cuando escriban en el input de password', () => {
+//       wrapper.find('input[name="username"]').simulate('change', {target: {name: 'password', value: 'My new value'}});
+//       expect(setState).toHaveBeenCalledWith({"username": "", "password": "My new value"});
+//     });
+//   });
+//
+//   describe('Validacion: ', () => {
+//     it('validate debe devolver un objeto con un error si el usarname no es un email valido:', () => {
+//       expect(validate({
+//         username: 'dassadas',
+//         password: 'hola1'
+//       })).toEqual({username: 'Username is invalid'});
+//     });
+//     it('validate debe devolver un objeto con un error si el usarname esta vacio:', () => {
+//       expect(validate({
+//         username: '',
+//         password: 'hola1',
+//       })).toEqual({username: 'Username is required'});
+//     });
+//     it('validate debe devolver un objeto con un error si el password no tiene un numero:', () => {
+//       expect(validate({
+//         username: 'toni@soyhenry.com',
+//         password: 'dassadas'
+//       })).toEqual({password: 'Password is invalid'});
+//     });
+//     it('validate debe devolver un objeto con un error si el password esta vacio:', () => {
+//       expect(validate({
+//         username: 'toni@soyhenry.com',
+//         password: ''
+//       })).toEqual({password: 'Password is required'});
+//     });
+//   });
+// });
--- 10-React-Forms/homework/src/setupTests.js
+++ 10-React-Forms/homework/src/setupTests.js
@@ -4,5 +4,5 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
 import * as enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 enzyme.configure({ adapter: new Adapter() });
