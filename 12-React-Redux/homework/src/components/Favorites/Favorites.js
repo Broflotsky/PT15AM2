1aa722fccea559bb3d7c64861eb2f14dfff469a2
Franco Etcheverri <francoetche@gmail.com>
2021-04-21 15:01:45 -0300

    Update Favorites.js

--- 12-React-Redux/homework/src/components/Favorites/Favorites.js
+++ 12-React-Redux/homework/src/components/Favorites/Favorites.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 // import { connect } from "react-redux";
-// import { removeArticle } from "../../actions/index";
 // import { Link } from 'react-router-dom';
 import './Favorites.css';
 
