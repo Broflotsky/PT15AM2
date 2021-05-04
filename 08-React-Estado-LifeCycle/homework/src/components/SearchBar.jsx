bb3bc8adf1d15aaac73a2dec700a3c772c3f6f5e
mscomazzon <mscomazzon@itba.edu.ar>
2021-05-04 11:45:30 -0300

    Update SearchBar.jsx
    removed state "city" and its management, setCity

--- 08-React-Estado-LifeCycle/homework/src/components/SearchBar.jsx
+++ 08-React-Estado-LifeCycle/homework/src/components/SearchBar.jsx
@@ -1,17 +1,14 @@
 import React, { useState } from "react";
 
 export default function SearchBar({onSearch}) {
-  const [city, setCity] = useState("");
   return (
     <form onSubmit={(e) => {
       e.preventDefault();
-      onSearch(city);
+      onSearch("Cairns");
     }}>
       <input
         type="text"
         placeholder="Ciudad..."
-        value={city}
-        onChange={e => setCity(e.target.value)}
       />
       <input type="submit" value="Agregar" />
     </form>
