978a89dcd07c1c77caf6c1821998647824e2c958
efedefede <63814886+efedefede@users.noreply.github.com>
2021-05-24 11:25:45 -0300

    Commented lines fixed

--- 12-React-Redux/demo-mutation/src/components/Counter.jsx
+++ 12-React-Redux/demo-mutation/src/components/Counter.jsx
@@ -1,49 +1,18 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux'
-import store from '../store.js';
-// import actionCreators from '../actions/index.js'
-import { addFriend } from '../actions/index.js';
-
- //
- // function incrementar() {
- //   store.dispatch(increment());
- // }
- //
- // function decrementar() {
- //   store.dispatch(decrement())
- // }
-
-// function mapDispatchToProps(dispatch) {
-//   return bindActionCreators(actionCreators, dispatch);
-// }
-
-// function mapDispatchToProps(dispatch) {
-//   return {
-//     incrementar: function () {
-//       dispatch(increment());
-//     },
-//     decrement: function (id) {
-//       dispatch(decrement())
-//     },
-//     reset: function(id) {
-//       dispatch(reset());
-//     },
-//   }
-// }
-
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import store from "../store.js";
+// import {increment, decrement, reset } from '../actions/index.js'
+import { addFriend } from "../actions/index.js";
 
 class Counter extends Component {
   render() {
     return (
       <div>
-        {
-          this.props.amigos && this.props.amigos.map((amigo, i) => (
-            <p key={i}>{amigo}</p>
-          ))
-        }
+        {this.props.amigos &&
+          this.props.amigos.map((amigo, i) => <p key={i}>{amigo}</p>)}
         <button onClick={this.props.addFriend}>ADD FRIEND TITO</button>
       </div>
-    )
+    );
   }
 }
 
@@ -53,15 +22,15 @@ class Counter extends Component {
 //       <p>
 //         Clicked: {this.props.count} times
 //         {' '}
-//         <button onClick={incrementar}>
+//         <button onClick={this.props.incrementar}>
 //           +
 //         </button>
 //         {' '}
-//         <button onClick={decrementar}>
+//         <button onClick={this.props.decrementar}>
 //           -
 //         </button>
 //         {' '}
-//         <button onClick={() => store.dispatch(reset())}>
+//         <button onClick={this.props.reset}>
 //           Reset
 //         </button>
 //       </p>
@@ -72,8 +41,22 @@ class Counter extends Component {
 const mapStateToProps = (state) => {
   return {
     count: state.count,
-    amigos: state.amigos
+    amigos: state.amigos,
   };
-}
+};
 
-export default connect(mapStateToProps, {addFriend})(Counter);
+// function mapDispatchToProps(dispatch) {
+//   return {
+//     incrementar: function () {
+//       dispatch(increment());
+//     },
+//     decrement: function () {
+//       dispatch(decrement())
+//     },
+//     reset: function() {
+//       dispatch(reset());
+//     },
+//   }
+// }
+
+export default connect(mapStateToProps, { addFriend })(Counter);
