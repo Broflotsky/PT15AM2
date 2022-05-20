f106f3441783e9d902c99e7049878bac59213c78
Ignacio Amatt <ignacio_amatt@hotmail.com>
2022-05-20 07:23:32 -0300

    React-Intro_OK

--- 06-React-Intro/homework/src/components/Card.jsx
+++ 06-React-Intro/homework/src/components/Card.jsx
@@ -1,4 +1,3 @@
-
 export default function Card (props) {
   // acá va tu código
   return (
--- 06-React-Intro/homework/src/components/SearchBar.jsx
+++ 06-React-Intro/homework/src/components/SearchBar.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export default function SearchBar (props) {
   // acá va tu código
   return (
