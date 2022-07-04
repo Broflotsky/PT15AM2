badd9a0fb282f80701bf3891b09cb29d9b21ded8
Ignacio Amatt <ignacio_amatt@hotmail.com>
2022-07-04 20:59:48 -0300

    Update Botones.jsx

--- 06-React-Intro/homework/01 - Exercises/src/components/Botones.jsx
+++ 06-React-Intro/homework/01 - Exercises/src/components/Botones.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 
 export default function Botones () {
-  return <div>Componente Botones</div>
+  return (
+    <div>
+      <button onClick={alert('mensaje 1')}>Botón 1</button>
+      <button onClick={alert('mensaje 2')}>Botón 2</button>
+    </div>
+  )
 }
