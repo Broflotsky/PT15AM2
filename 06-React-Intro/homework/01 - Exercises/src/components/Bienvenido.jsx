58948798c8da7c1ed842645956adec4ae2eb1aee
Ignacio Amatt <ignacio_amatt@hotmail.com>
2022-07-11 11:40:12 -0300

    cleaning solutions

--- 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
+++ 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
@@ -1,23 +1,11 @@
 import React from 'react'
-import Botones from './Botones'
 
-const studentName = 'Nacho'
+const studentName = ''
 const techSkills = ['Html', 'Css', 'JavaScript', 'React', 'Redux']
 const alerts = { m1: 'Aprobado', m2: 'En curso' }
 
 export default function Bienvenido () {
-  return (
-    <div>
-      <h1>Bienvenido a React</h1>
-      <h3>{studentName}</h3>
-      <ul>
-        {techSkills.map(skill => (
-          <li key={skill}>{skill}</li>
-        ))}
-      </ul>
-      <Botones alerts={alerts} />
-    </div>
-  )
+  // el código de tu componente acá
 }
 
 // Esto lo exportamos para los tests
