cc4e998294cffa41cd70608c0dff5f72c17ffe9f
MPrecelle <tinchoprecelle@gmail.com>
2022-07-18 15:23:20 -0300

    Fix Bienvenido.jsx

--- 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Bienvenido/Bienvenido.jsx
+++ 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Bienvenido/Bienvenido.jsx
@@ -1,27 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import Botones from '../Botones/Botones.jsx';
-import styles from './Bienvenido.module.css';
+import React, { useState, useEffect } from "react";
+import Botones from "../Botones/Botones.jsx";
+import styles from "./Bienvenido.module.css";
 
 const studentName = "Nombre alumno";
-const techSkills = [
- 
-];
+const techSkills = [];
 const alerts = { m1: "Aprobado", m2: "En curso" };
 
 export default function Bienvenido() {
   return (
-    <div >
-      <h1 style={{ fontStyle: "italic", fontSize: "50px" }}>
-        soy Henry!
-      </h1>
-      <h3 >{studentName}</h3>
-      <ul >
-        {/* {techSkills.map((skill) => (
+    <div>
+      <h1 style={{ fontStyle: "italic", fontSize: "50px" }}>soy Henry!</h1>
+      <h3>{studentName}</h3>
+      <ul>
+        {techSkills.map((skill) => (
           <li className={s.itemsList} key={skill}>
             {skill.tech}
             <img src={skill.image} alt={skill.tech} />
           </li>
-        ))} */}
+        ))}
       </ul>
       <Botones alerts={alerts} />
     </div>
