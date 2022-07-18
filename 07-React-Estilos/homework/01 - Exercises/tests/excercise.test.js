6877b1637b5ed93997b0fface2fb12b767337902
mate444 <mateoeo23@gmail.com>
2022-07-18 10:19:05 -0300

    Improved css module test

--- 07-React-Estilos/homework/01 - Exercises/tests/excercise.test.js
+++ 07-React-Estilos/homework/01 - Exercises/tests/excercise.test.js
@@ -4,6 +4,7 @@ import '@testing-library/jest-dom/extend-expect'
 import 'jest-styled-components'
 import { shallow, configure } from 'enzyme'
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
+import styles from '../src/components/Bienvenido/Bienvenido.module.css';
 // Importamos variables/componentes
 import Bienvenido from '../src/components/Bienvenido/Bienvenido.jsx'
 import Botones, {
@@ -22,22 +23,22 @@ describe("01 | Componente 'Bienvenido'", () => {
 
   it("La etiqueta div debe contener una clase llamada 'divBienvenido'", () => {
     const divBienvenido = wrapperBienvenido.find('div')
-    expect(divBienvenido.hasClass('divBienvenido')).toBe(true)
+    expect(divBienvenido.hasClass(styles.divBienvenido)).toBe(true)
   })
 
   it("La etiqueta h1 debe contener una clase llamada 'title'", () => {
     const h1 = wrapperBienvenido.find('h1')
-    expect(h1.hasClass('title')).toBe(true)
+    expect(h1.hasClass(styles.title)).toBe(true)
   })
 
   it("La etiqueta h3 debe contener una clase llamada 'subtitle'", () => {
     const h3 = wrapperBienvenido.find('h3')
-    expect(h3.hasClass('subtitle')).toBe(true)
+    expect(h3.hasClass(styles.subtitle)).toBe(true)
   })
 
   it("La etiqueta ul debe contener una clase llamada 'unorderedList'", () => {
     const ul = wrapperBienvenido.find('ul')
-    expect(ul.hasClass('unorderedList')).toBe(true)
+    expect(ul.hasClass(styles.unorderedList)).toBe(true)
   })
 
   it("Las etiquetas li debe contener una clase llamada 'listItem'", () => {
