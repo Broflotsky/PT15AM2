81c4377037f2a5e3d9d079f95efd57ae3dd90310
Ignacio Amatt <ignacio_amatt@hotmail.com>
2022-07-25 13:26:27 -0300

    linted

--- 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Animals/Animals.jsx
+++ 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Animals/Animals.jsx
@@ -1,33 +1,33 @@
-import React from "react";
+import React from 'react'
 
 export default class Animals extends React.Component {
-  constructor(props){
-    super(props);
+  constructor (props) {
+    super(props)
     this.state = {
-      totalAnimals:null
+      totalAnimals: null
     }
   }
-  componentWillUpdate(prevProps, prevStates){
-    if(prevStates.totalAnimals !== prevProps.animals.length){
-      this.setState({totalAnimals: this.props.animals.length})
-    }
-  }
-  
 
-  render() {
+  componentWillUpdate (prevProps, prevStates) {
+    if (prevStates.totalAnimals !== prevProps.animals.length) {
+      this.setState({ totalAnimals: this.props.animals.length })
+    }
+  }
+
+  render () {
     const { animals } = this.props
     return (
       <div>
-          <h2>{this.state.totalAnimals}</h2>
-          {animals?.map((animal, key) => (
-            <div key={key}>
-              <h5>{animal.name}</h5>
-              <img src={animal.image} alt="no puedo mostrar " width="300px"/>
-              <span>{animal.specie}</span>
-            </div>
-          ))}
-        
+        <h2>{this.state.totalAnimals}</h2>
+        {animals?.map((animal, key) => (
+          <div key={key}>
+            <h5>{animal.name}</h5>
+            <img src={animal.image} alt='no puedo mostrar ' width='300px' />
+            <span>{animal.specie}</span>
+          </div>
+        ))}
+
       </div>
-    );
+    )
   }
 }
