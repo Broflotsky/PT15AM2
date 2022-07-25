a42581148353d065ec8e0e66953a863408e7f9e7
Ignacio Amatt <ignacio_amatt@hotmail.com>
2022-07-25 13:25:26 -0300

    linted

--- 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Zoo/Zoo.jsx
+++ 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Zoo/Zoo.jsx
@@ -1,38 +1,38 @@
-import React from "react";
-import Animals from "../Animals/Animals";
-import Species from "../Species/Species";
-import styles from "./Zoo.module.css";
+import React from 'react'
+import Animals from '../Animals/Animals'
+import Species from '../Species/Species'
+import styles from './Zoo.module.css'
 
-export default function Zoo() {
+export default function Zoo () {
   const [zoo, setZoo] = React.useState({
-    zooName: "",
+    zooName: '',
     animals: [],
     copyAnimals: [],
-    species: [],
-  });
+    species: []
+  })
 
   React.useEffect(() => {
-    fetch("http://localhost:3001/animals")
+    fetch('http://localhost:3001/animals')
       .then((res) => res.json())
       .then((data) =>
-        setZoo({ ...zoo, animals: data.animals, species: data.species, copyAnimals:data.animals })
+        setZoo({ ...zoo, animals: data.animals, species: data.species, copyAnimals: data.animals })
       )
-      .catch((error) => console.log(error));
-  }, []);
+      .catch((error) => console.log(error))
+  }, [])
 
   const handleInputChange = (e) => {
     setZoo({
       ...zoo,
-      zooName: e.target.value,
-    });
-  };
+      zooName: e.target.value
+    })
+  }
 
   const handleSpecies = (e) => {
     setZoo({
       ...zoo,
       animals: zoo.copyAnimals.filter(a => a.specie.toLowerCase() === e.target.value.toLowerCase())
-    });
-  };
+    })
+  }
   return (
     <div>
       <h1 className={styles.title}>Mi Zoo!</h1>
@@ -41,13 +41,13 @@ export default function Zoo() {
         className={styles.input}
         value={zoo.zooName}
         onChange={handleInputChange}
-      ></input>
+      />
       <h3 className={styles.subtitle}>{zoo.zooName}</h3>
       <div className={styles.containerComponents}>
         {/* Escribe acá tu código */}
-        <Species species={zoo.species} handleSpecies={handleSpecies}/>
-        <Animals animals={zoo.animals}/>
+        <Species species={zoo.species} handleSpecies={handleSpecies} />
+        <Animals animals={zoo.animals} />
       </div>
     </div>
-  );
+  )
 }
