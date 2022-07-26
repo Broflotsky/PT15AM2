df21dabd4bad08f8b3f8f5a1debae056c702fee3
MPrecelle <tinchoprecelle@gmail.com>
2022-07-26 13:44:59 -0300

    Fix Zoo.jsx and eslint-disabled in useEffect dependency

--- 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Zoo/Zoo.jsx
+++ 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Zoo/Zoo.jsx
@@ -2,33 +2,39 @@ import React from "react";
 import Animals from "../Animals/Animals";
 import Species from "../Species/Species";
 
-export default function Zoo () {
+export default function Zoo() {
   /* Escribe acá tu código */
   const [zoo, setZoo] = React.useState({
     zooName: "",
     animals: [],
     species: [],
-    copyAnimals: []
+    copyAnimals: [],
   });
 
-  React.useEffect(() => {
-    fetch("http://localhost:3001/animals")
-      .then((res) => res.json())
-      .then((data) =>
-        setZoo({ ...zoo, 
-        animals: data.animals, 
-        species: data.species, 
-        copyAnimals:data.animals })
-      )
-      .catch((error) => console.log(error));
-  })
+  React.useEffect(
+    () => {
+      fetch("http://localhost:3001/animals")
+        .then((res) => res.json())
+        .then((data) =>
+          setZoo({
+            ...zoo,
+            animals: data.animals,
+            species: data.species,
+            copyAnimals: data.animals,
+          })
+        )
+        .catch((error) => console.log(error));
+    },
+    // eslint-disable-next-line
+    []
+  );
 
-  function handleInputChange(e){
+  function handleInputChange(e) {
     setZoo({
       ...zoo,
       zooName: e.target.value,
     });
-  };
+  }
 
   const handleSpecies = (e) => {
     setZoo({
@@ -41,11 +47,11 @@ export default function Zoo () {
   return (
     <div>
       <label>Zoo Name:</label>
-      <input value={zoo.zooName} onChange={handleInputChange}/>
+      <input value={zoo.zooName} onChange={handleInputChange} />
       <h1>{zoo.zooName}</h1>
-      <div >
-        <Species species={zoo.species} handleSpecies={handleSpecies}/>
-        <Animals animals={zoo.animals}/>
+      <div>
+        <Species species={zoo.species} handleSpecies={handleSpecies} />
+        <Animals animals={zoo.animals} />
       </div>
     </div>
   );
