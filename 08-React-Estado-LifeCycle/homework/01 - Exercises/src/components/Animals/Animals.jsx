8e4ce973dbab5c112ad1a8c34db47830d821b55f
aurasandoval <auri.sandoval@gmail.com>
2022-07-26 13:07:52 -0300

    refactor exercise 1,2 y 3

--- 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Animals/Animals.jsx
+++ 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Animals/Animals.jsx
@@ -1,29 +1,22 @@
 import React from 'react'
 
 export default class Animals extends React.Component {
-  constructor(props){
-   super()
-  }
-  
-
-  componentWillUpdate (prevProps, prevStates) {
-    if (prevStates.totalAnimals !== prevProps.animals.length) {
-      this.setState({ totalAnimals: this.props.animals.length })
-    }
-  }
+  // constructor(props){
+  //  super()
+  // }
 
   render () {
-    const { animals } = this.props
+    // const { animals } = this.props
     return (
       <div>
-          {animals?.map((animal, key) => (
+          {/* {animals?.map((animal, key) => (
             <div key={key}>
               <h5>{animal.name}</h5>
               <img src={animal.image} alt="no puedo mostrar " width="300px"/>
               <span>{animal.specie}</span>
             </div>
           ))}
-        
+         */}
       </div>
     )
   }
