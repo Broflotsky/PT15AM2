78507ba0602cb6eab874be0faa28867aa0f38231
MPrecelle <tinchoprecelle@gmail.com>
2022-07-26 13:15:21 -0300

    Fix testing

--- 08-React-Estado-LifeCycle/homework/01 - Exercises/tests/04.test.js
+++ 08-React-Estado-LifeCycle/homework/01 - Exercises/tests/04.test.js
@@ -1,6 +1,5 @@
-import React from "react";
 import "@testing-library/jest-dom/extend-expect";
-import { shallow, configure } from "enzyme";
+import { configure } from "enzyme";
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 import isReact from "is-react";
 import Animals from "../src/components/Animals/Animals";
@@ -8,7 +7,7 @@ import Animals from "../src/components/Animals/Animals";
 configure({ adapter: new Adapter() });
 
 describe("04 | Ejercicios", () => {
-  let animals;
+  // let animals;
   beforeAll(() => expect(isReact.classComponent(Animals)).toBeTruthy());
 
 });
