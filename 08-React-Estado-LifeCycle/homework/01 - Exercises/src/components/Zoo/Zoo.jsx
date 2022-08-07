d063b15a4acab547df6da261eeecba0033565896
Ignacio Amatt <ignacio_amatt@hotmail.com>
2022-08-07 20:08:54 -0300

    linting

--- 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Zoo/Zoo.jsx
+++ 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Zoo/Zoo.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import Animals from "../Animals/Animals";
-import Species from "../Species/Species";
+import React from 'react'
+import Animals from '../Animals/Animals'
+import Species from '../Species/Species'
 
-export default function Zoo() {
+export default function Zoo () {
   /* Escribe acá tu código */
   return (
     <div>
       <h1>-</h1>
     </div>
-  );
+  )
 }
