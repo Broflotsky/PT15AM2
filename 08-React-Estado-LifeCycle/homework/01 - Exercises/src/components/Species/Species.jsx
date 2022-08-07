2efe6a8ff0b3565783304882300f33f0561b9a53
Ignacio Amatt <ignacio_amatt@hotmail.com>
2022-08-07 20:55:02 -0300

    linting

--- 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Species/Species.jsx
+++ 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Species/Species.jsx
@@ -1,5 +1,9 @@
-import React from "react";
+import React from 'react'
 
-export default function Species() {
-  return <div></div>;
+export default function Species () {
+  return (
+    <div>
+
+    </div>
+  )
 }
