b78569adf1a37aa4aff6ef2929612182c24bb39a
aurasandoval <auri.sandoval@gmail.com>
2022-08-10 11:02:38 -0300

    fix db.json change property

--- 09-React-Routing/homework/01 - Exercises/src/components/Card/Card.jsx
+++ 09-React-Routing/homework/01 - Exercises/src/components/Card/Card.jsx
@@ -1,6 +1,7 @@
 import styleCard from "./Card.module.css";
 
-export default function Card({ name, image }) {
+export default function Card({ name, image, itinerary }) {
+  console.log(itinerary.da);
   return (
     <div className={styleCard.container}>
       <h4>{name}</h4>
