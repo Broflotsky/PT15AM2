a2a1e60d0661c69bc62e2d2a46c89b90f5f3252e
MPrecelle <tinchoprecelle@gmail.com>
2022-08-30 12:17:44 -0300

    Finish testings exercises 1, 2 & 3

--- 10-React-Forms/homework/01 - Exercises/tests/02.test.js
+++ 10-React-Forms/homework/01 - Exercises/tests/02.test.js
@@ -2,9 +2,9 @@
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
 import { configure, shallow } from "enzyme";
-import Contact from "../src/components/Contact/Contact";
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 // Importamos variables/componentes
+import Contact from "../src/components/Contact/Contact";
 
 configure({ adapter: new Adapter() });
 
