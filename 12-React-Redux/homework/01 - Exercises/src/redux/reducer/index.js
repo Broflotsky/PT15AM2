f54c80ddeee2380cf57bc5c6fd10e4e7362f3b6f
AlejoBengo <alejobengo@gmail.com>
2022-11-09 14:15:55 -0300

    d

--- 12-React-Redux/homework/01 - Exercises/src/redux/reducer/index.js
+++ 12-React-Redux/homework/01 - Exercises/src/redux/reducer/index.js
@@ -1,5 +1,25 @@
-// const initialState = {
-//   list: []
-// }
+import { DELETE_PRODUCT, ADD_PRODUCT } from '../actions/types';
 
-// export default rootReducer
+const initialState = {
+   list: [],
+};
+
+const rootReducer = (state = initialState, { type, payload }) => {
+   switch (type) {
+      case ADD_PRODUCT:
+         let aca = [...state.list];
+         aca.push(payload);
+         return { ...state, list: aca };
+
+      case DELETE_PRODUCT:
+         let aca2 = [...state.list].filter((p) => {
+            return p.id !== payload;
+         });
+         return { ...state, list: aca2 };
+
+      default:
+         return state;
+   }
+};
+
+export default rootReducer;
