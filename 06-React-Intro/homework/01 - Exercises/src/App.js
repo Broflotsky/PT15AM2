0cd413db7f653ff5dd85a26bb1df94a5f31ae0e2
AlejoBengo <alejobengo@gmail.com>
2022-12-01 14:27:58 -0300

    Nuevos scripts en los ejercicios

--- 06-React-Intro/homework/01 - Exercises/src/App.js
+++ 06-React-Intro/homework/01 - Exercises/src/App.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import Bienvenido from './components/Bienvenido.jsx'
-
-export default function App () {
-  return (
-    <div>
-      <Bienvenido />
-    </div>
-  )
+import React from 'react';
+import Bienvenido from './components/Bienvenido.jsx';
+process.versions.node;
+export default function App() {
+   return (
+      <div>
+         <Bienvenido />
+      </div>
+   );
 }
