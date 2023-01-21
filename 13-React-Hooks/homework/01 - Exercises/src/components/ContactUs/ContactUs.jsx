e2f1392f1173556854668bcca6a7c904b771c1af
aurasandoval <auri.sandoval@gmail.com>
2023-01-20 23:29:26 -0300

    editing hws 9 y 13

--- 13-React-Hooks/homework/01 - Exercises/src/components/ContactUs/ContactUs.jsx
+++ 13-React-Hooks/homework/01 - Exercises/src/components/ContactUs/ContactUs.jsx
@@ -2,12 +2,18 @@ import React from "react";
 
 const ContactUs = () => {
   return (
-    <div className="contactBg">
-      <input name="nombre"></input>
-      <input name="email"></input>
-      <input name="asunto"></input>
-      <input name="mensaje"></input>
-      <button>ENVIAR</button>
+    <div>
+      <form className="contactBg">
+        <label htmlFor="nombre">Nombre: </label>
+        <input name="nombre" />
+        <label htmlFor="email">Email: </label>
+        <input name="email" />
+        <label htmlFor="asunto">Asunto: </label>
+        <input name="asunto" />
+        <label htmlFor="mensaje">Mensaje: </label>
+        <input name="mensaje" />
+        <button>Enviar</button>
+      </form>
     </div>
   );
 };
